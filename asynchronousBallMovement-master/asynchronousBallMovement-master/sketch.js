0e35d143d5a9cfdb21145d9a6f66aa123e70b354
Aadil <72676882+aadilMohamed@users.noreply.github.com>
2020-12-13 09:55:54 +0800

    Add files via upload

---
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,48 @@
+var ball,database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var listenersRequest = database.ref('ball/position')
+    listenersRequest.on("value",ballPosition)
+
+}
+
+function draw(){
+    background("white");
+
+    if(position!==undefined){
+
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+
+function changePosition(x,y){
+   database.ref('ball/position').set({
+       "x":position.x+x,
+       "y":position.y+y
+   })
+}
+
+function ballPosition(data){
+position = data.val()
+ball.x = position.x
+ball.y = position.y
+}
